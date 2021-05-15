71017a24d2b9ceb5ab7f9429664b17acfba46e2b
BennettSchneiderANU <bennett.schneider@anu.edu.au>
2021-05-15 15:14:54 +1000

    updated fcas_reg_stats.py to include calculation of revenue-weighted Fr distributions

--- testing/fcas_reg_stats.py
+++ testing/fcas_reg_stats.py
@@ -27,8 +27,8 @@ import h5py
 
 
 #%% Load dependencies
-rrp_file = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\prices\20210505_NEM_20200701-00-05_20210301-00-00_RRP.csv"
-dispatchload_file = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\dispatchload\20210507_NEM_20200701-00-05_20210301-00-00_DISPATCHLOAD.csv"
+rrp_file = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\prices\20210514_NEM_20190501-00-05_20210501-00-00_RRP.csv"
+dispatchload_file = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\dispatchload\20210514_NEM_20190501-00-05_20210501-00-00_DISPATCHLOAD.csv"
 fcas_reg_file = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\fcas_reg\reg_component_5min.csv"
 export = r"C:\Users\benne\OneDrive - Australian National University\Master of Energy Change\SCNC8021\packaging_working\results"
 
@@ -79,10 +79,12 @@ data['RAISE_frac'] = (data[data['GenRegComp_MW'] > 0]['GenRegComp_MW']/data['RAI
 data['LOWER_frac'] = (data[data['GenRegComp_MW'] < 0]['GenRegComp_MW']/data['LOWERREG']).replace(np.inf,np.nan)
 
 # Clean up bad values
-data.loc[data['RAISE_frac'].abs() > 10,'RAISE_frac'] = np.nan
-data.loc[data['LOWER_frac'].abs() > 10,'LOWER_frac'] = np.nan
+data.loc[data['RAISE_frac'].abs() > 1,'RAISE_frac'] = np.nan
+data.loc[data['LOWER_frac'].abs() > 1,'LOWER_frac'] = np.nan
 
 #%% Add intervals
+
+# Price intervals
 bins = {
         'RRP':(-1000,-300,-100,0,100,300,1000,5000,15000),
         'RAISEREGRRP':(0,100,300,1000,5000,15000),
@@ -95,15 +97,36 @@ for col in [col for col in data.columns if 'RRP' in col]:
     except KeyError:
         pass
 
-data['Hour'] = pd.DatetimeIndex(data['Timestamp']).hour
+
+# Fr intervals
+for market in ['RAISE','LOWER']:
+    # Set market_frac to 0 if nan during a period where the duid is enabled in the corresponding market
+    data.loc[data[f'{market}REG']>0,f'{market}_frac'] = data[f'{market}_frac'].fillna(0)
+    
+    data.loc[abs(data[f'{market}_frac']) < 0.01,f'{market}_frac'] = 0 # set really small values to 0 to clean up bins
+    # data[f'{market}_frac_bins'] = pd.Series(list(pd.cut(data[f'{market}_frac'],bins=10,right=False))).astype(str)
+    data[f'{market}_frac_bins'] = pd.IntervalIndex(pd.cut(data[f'{market}_frac'],bins=10,right=False)).mid
+
+# Calculate revenue due to regulation energy dispatch in RAISE and LOWER respectively
+data['RAISE_Energy_Revenue'] = data['RAISE_frac']*data['RAISEREG']*data['RRP']
+data['LOWER_Energy_Revenue'] = data['LOWER_frac']*data['LOWERREG']*data['RRP']
+
+# Calculate the maximum total revenue due to regulation dispatch in RAISE and LOWER respectively
+data['RAISE_Energy_Revenue_max'] = data['RAISEREG']*data['RRP']
+data['LOWER_Energy_Revenue_max'] = -data['LOWERREG']*data['RRP']
+
+
+data['Hour'] = 3*(pd.DatetimeIndex(data['Timestamp']).hour//3)
+data['Month'] = pd.DatetimeIndex(data['Timestamp']).month
 
 # Flag data before/after Oct 1 2020 (introduction of MPF)
 data['MPF'] = 'No'
 data.loc[data['Timestamp'] > dt.datetime(2020,10,1),'MPF'] = 'Yes'
+data_crop = data.copy()[data['Month'].isin([10,11,12,1,2])]
 
 #%% Process data to plot
-multiindex = ['Timestamp','duid pre','Hour']
-toPlot = data.copy().set_index(multiindex)[['RAISEREG','LOWERREG','RAISE_frac','LOWER_frac','GenRegComp_MW']].stack().reset_index().rename({f'level_{len(multiindex)}':'Variable',0:'Value'},axis=1)
+multiindex = ['Timestamp','duid pre','Hour','Month']
+toPlot = data_crop.copy().set_index(multiindex)[['RAISEREG','LOWERREG','RAISE_frac','LOWER_frac','GenRegComp_MW']].stack().reset_index().rename({f'level_{len(multiindex)}':'Variable',0:'Value'},axis=1)
 # Flag data before/after Oct 1 2020 (introduction of MPF)
 toPlot['MPF'] = 'No'
 toPlot.loc[toPlot['Timestamp'] > dt.datetime(2020,10,1),'MPF'] = 'Yes'
@@ -114,7 +137,8 @@ plot(fig)
 #%% Plot histogram
 toPlot_hist = toPlot[toPlot['Variable'].isin(['RAISE_frac','LOWER_frac'])]
 
-fig = px.histogram(toPlot_hist,x='Value',color='duid pre',facet_row='Variable',facet_col='Hour',width=3000).update_yaxes(matches=None).update_layout(xaxis={'range':[-1,1]})
+fig = px.histogram(toPlot_hist,x='Value',color='duid pre',facet_row='Variable',facet_col='MPF').update_yaxes(matches=None).update_layout(xaxis={'range':[-1,1]})
+fig.write_html(os.path.join(export,f"mpf_Fr_hist.html"))
 plot(fig)
 
 #%% Interval box plot
@@ -122,26 +146,63 @@ multiindex = ['Timestamp','duid pre','RRP_interval']
 toPlot_box = data.copy().set_index(multiindex)[['RAISE_frac','LOWER_frac']].stack().reset_index().rename({f'level_{len(multiindex)}':'Variable',0:'Value'},axis=1)
 fig = px.box(toPlot_box,x='RRP_interval',y='Value',color='duid pre',facet_row='Variable') # no price/Fr relationship
 plot(fig)
-#%% Plot fraction against price
-fig = px.scatter(data,x='RRP',y='LOWER_frac',color='duid pre')
+#%% Plot values against one another
+fig = px.scatter(data,x='RRP',y='RAISEREG',color='duid pre')
 plot(fig)
 
 #%% Plot diurnal bar chart
-toPlot_diurnal = toPlot.copy()
-toPlot_diurnal['Hour'] = pd.DatetimeIndex(toPlot_diurnal['Timestamp']).hour
-fig = px.box(toPlot_diurnal,x='Hour',y='Value',color='duid pre',facet_row='Variable').update_yaxes(matches=None)
+# toPlot_diurnal = toPlot.copy()[toPlot['Variable'].isin(['LOWER_frac','RAISE_frac'])]
+# facet_col = 'Month'
+# # toPlot_diurnal['Hour'] = pd.DatetimeIndex(toPlot_diurnal['Timestamp']).hour
+# fig = px.histogram(
+#     toPlot_diurnal,
+#     x='Value',
+#     color='MPF',
+#     facet_row='Variable',
+#     facet_col=facet_col,
+#     barmode='group',
+#     nbins=20,
+#     histnorm  = 'probability density'
+#     )# .update_yaxes(matches=None)
+# fig.write_html(os.path.join(export,f"mpf_Fr_hist_{facet_col}.html"))
+# plot(fig)
+
+# #%% Enablement-weighted diurnal average Fr 
+# multiindex = ['duid pre','Hour','MPF']
+# data['wRAISE_frac'] = data['RAISEREG']*data['RAISE_frac']
+# Fr_raise = (data.groupby(multiindex)['wRAISE_frac'].sum()/data.groupby(multiindex)['RAISEREG'].sum()).reset_index().rename({0:'RAISE'},axis=1)
+
+# data['wLOWER_frac'] = data['LOWERREG']*data['LOWER_frac']
+# Fr_lower = (data.groupby(multiindex)['wLOWER_frac'].sum()/data.groupby(['duid pre','Hour'])['LOWERREG'].sum()).reset_index().rename({0:'LOWER'},axis=1)
+
+# Fr = pd.merge(Fr_raise,Fr_lower,how='inner',on=multiindex).set_index(multiindex).stack().reset_index().rename({f'level_{len(multiindex)}':'Market',0:'Value'},axis=1)
+
+# fig = px.line(Fr,x='Hour',y='Value',color='duid pre',facet_col='Market',facet_row='MPF').update_yaxes(matches=None)
+# plot(fig)
+# fig.write_html(os.path.join(export,'hourly_Fr.html'))
+
+#%% Calculate revenue-weighted average Fr, as per the notes
+# Cut this across any pre-defined set of groupings
+
+group = ['Hour','MPF'] # variable apart from Fr_bins you want to group by
+
+def calc_bn(data_group,market,group):
+    group_dist = copy.deepcopy(group)
+    group_dist.append(f'{market}_frac_bins') # group by the original group, plus the Fr bins
+    # apply new grouping only to the numerator
+    bn = data_group.groupby(group_dist)[f'{market}_Energy_Revenue_max'].sum()/data_group[f'{market}_Energy_Revenue_max'].sum()
+    # set the index so it gels with the caller
+    bn = bn.reset_index().drop(group,axis=1).set_index(f'{market}_frac_bins')
+    bn.columns = ['bn']
+    return bn
+
+bn = pd.DataFrame()
+for market in ['RAISE','LOWER']: # for each market
+    # Calculate F_T distribution across a number of different groupings
+    bn_part = data.groupby(group).apply(lambda x: calc_bn(x,market,group)).reset_index().rename({f'{market}_frac_bins':'Fr_bins'},axis=1)
+    bn_part['Market'] = market
+    bn = bn.append(bn_part)
+
+#%% Plot revenue-weighted average
+fig = px.bar(bn,x='Fr_bins',y='bn',facet_row='Hour',facet_col='MPF',color='Market',barmode='relative').update_xaxes(matches=None)
 plot(fig)
-
-#%% Enablement-weighted diurnal average Fr 
-multiindex = ['duid pre','Hour','MPF']
-data['wRAISE_frac'] = data['RAISEREG']*data['RAISE_frac']
-Fr_raise = (data.groupby(multiindex)['wRAISE_frac'].sum()/data.groupby(multiindex)['RAISEREG'].sum()).reset_index().rename({0:'RAISE'},axis=1)
-
-data['wLOWER_frac'] = data['LOWERREG']*data['LOWER_frac']
-Fr_lower = (data.groupby(multiindex)['wLOWER_frac'].sum()/data.groupby(['duid pre','Hour'])['LOWERREG'].sum()).reset_index().rename({0:'LOWER'},axis=1)
-
-Fr = pd.merge(Fr_raise,Fr_lower,how='inner',on=multiindex).set_index(multiindex).stack().reset_index().rename({f'level_{len(multiindex)}':'Market',0:'Value'},axis=1)
-
-fig = px.line(Fr,x='Hour',y='Value',color='duid pre',facet_col='Market',facet_row='MPF').update_yaxes(matches=None)
-plot(fig)
-fig.write_html(os.path.join(export,'hourly_Fr.html'))
